4bed236bcba38b306adc62cbafa18f2ec40c5215
zero1036 <t799162239@hotmail>
2015-04-15 18:41:14 +0800

    临时更新

--- WebContent/scripts/controllers/loginCtrl.js
+++ WebContent/scripts/controllers/loginCtrl.js
@@ -1,9 +1,17 @@
 define([ 'controllers/controllers' ], function(controllers) {
-	controllers.controller('loginCtrl', [ '$rootScope', '$scope',
-			function($rootScope, $scope) {
+	controllers.controller('loginCtrl', [ '$rootScope', '$http', '$scope',
+			function($rootScope, $http, $scope) {
 				$scope.homePageUrl = 'index.html';
 				$scope.User = {};
-				$scope.user = { account: 'superadmin', password: '123' };
+				$scope.user = {
+					account : 'superadmin',
+					password : '123'
+				};
+				$scope.test = function() {
+					$http.post("/test1").success(function(data) {
+						alert(data);
+					});
+				};
 
 			} ]);
 });
--- src/test1.java
+++ src/test1.java
@@ -1,10 +1,10 @@
-
 import java.io.*;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.sina.sae.util.SaeUserInfo;
 
 /**
  * Servlet implementation class test1
@@ -12,31 +12,55 @@ import javax.servlet.http.HttpServletResponse;
 public class test1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public test1() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// 设置响应内容类型
-	      response.setContentType("text/html");
-
-	      // 实际的逻辑是在这里
-	      PrintWriter out = response.getWriter();
-	      out.println("<h1>" + "fuck that" + "</h1>");
+	public test1() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		String appName = SaeUserInfo.getAppName();
+		String appAccessKey =	SaeUserInfo.getAccessKey();
+		String appSecretKey =	SaeUserInfo.getSecretKey();
+		String appAppVersion =	SaeUserInfo.getAppVersion();
+		String appSaeTmpPath =	SaeUserInfo.getSaeTmpPath();
+		
+		String strOP = "Name" + appName + ";AccessKey" + appAccessKey;
+		// 设置响应内容类型
+		response.setContentType("text/html");
+		// 实际的逻辑是在这里
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + strOP + "</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String appName = SaeUserInfo.getAppName();
+		String appAccessKey =	SaeUserInfo.getAccessKey();
+		String appSecretKey =	SaeUserInfo.getSecretKey();
+		String appAppVersion =	SaeUserInfo.getAppVersion();
+		String appSaeTmpPath =	SaeUserInfo.getSaeTmpPath();
+		
+		String strOP = "Name" + appName + ";AccessKey" + appAccessKey;
+		// 设置响应内容类型
+		response.setContentType("text/html");
+		// 实际的逻辑是在这里
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + strOP + "</h1>");
 	}
 
 }
